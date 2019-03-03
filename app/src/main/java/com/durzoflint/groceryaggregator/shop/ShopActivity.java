0e4a5568ce7a9718f9f10f32d710f473e2cb8919
durzoflint <abhinavu1201@gmail.com>
2019-03-03 17:54:35 +0530

    Added Shop Design

--- app/src/main/java/com/durzoflint/groceryaggregator/shop/ShopActivity.java
+++ app/src/main/java/com/durzoflint/groceryaggregator/shop/ShopActivity.java
@@ -1,4 +1,4 @@
-package com.durzoflint.groceryaggregator.home;
+package com.durzoflint.groceryaggregator.shop;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,63 +10,29 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
 
 import com.durzoflint.groceryaggregator.R;
 import com.durzoflint.groceryaggregator.login.LoginActivity;
-import com.durzoflint.groceryaggregator.shop.ShopActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ShopActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.NavigationDrawerTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_shop);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_action_menu);
+        setTitle("");
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null) {
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-        }
-
-        LinearLayout shop1 = findViewById(R.id.shop1);
-        shop1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this, ShopActivity.class));
-            }
-        });
-
-        LinearLayout shop2 = findViewById(R.id.shop2);
-        shop2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this, ShopActivity.class));
-            }
-        });
-
-        LinearLayout shop3 = findViewById(R.id.shop3);
-        shop3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this, ShopActivity.class));
-            }
-        });
     }
 
     @Override
